b4c6291530d86f57b6c5cc9f346b76a3ba6b7805
Abhiraj Shourya <abhirajshourya@gmail.com>
2023-05-29 22:01:31 +0530

    init

---
+++ backend/index.ts
@@ -0,0 +1,28 @@
+import Hapi from '@hapi/hapi';
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: () => {
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./App.css");
+function App() {
+    return (<p>
+      Hello World
+    </p>);
+}
+exports.default = App;
