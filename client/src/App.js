2354b35562d250e2283b099524df599312b9fb85
Abhiraj Shourya <abhirajshourya@gmail.com>
2023-05-30 17:25:58 +0530

    hygiene

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./App.css");
-function App() {
-    return (<p>
-      Hello World
-    </p>);
-}
-exports.default = App;
