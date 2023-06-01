157f8ae90589d6dadd3451d73b9ebe4288ce7ed1
Abhiraj Shourya <abhirajshourya@gmail.com>
2023-06-01 16:19:52 +0530

    Theme Switch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,23 @@
 import Tracker from './components/Tracker';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Box, Container, Divider, Grid, Typography } from '@mui/material';
+import { Box, Container, Divider, Grid, IconButton, Switch, Typography } from '@mui/material';
 import Entry from './components/Entry';
 import { useEffect, useState } from 'react';
 import { getEntries } from './routes/routes';
 import { formatTimestamp } from './utils/formatTime';
+import React from 'react';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 const darkTheme = createTheme({
 	palette: {
 		mode: 'dark',
+	},
+});
+const lightTheme = createTheme({
+	palette: {
+		mode: 'light',
 	},
 });
 
@@ -22,6 +30,18 @@ export interface TimeEntryModel {
 function App() {
 	const [entries, setEntries] = useState<TimeEntryModel[]>([]);
 	const [refresh, setRefresh] = useState(true);
+	const [theme, setTheme] = useState(lightTheme);
+
+	const colorMode = React.useMemo(
+		() => ({
+			toggleColorMode: () => {
+				setTheme((prevMode) =>
+					prevMode.palette.mode === theme.palette.mode ? darkTheme : lightTheme
+				);
+			},
+		}),
+		[]
+	);
 
 	useEffect(() => {
 		const getTimeEntries = async () => {
@@ -33,17 +53,21 @@ function App() {
 	}, [refresh]);
 
 	return (
-		<ThemeProvider theme={darkTheme}>
+		<ThemeProvider theme={theme}>
 			<CssBaseline />
-			<Box sx={{ m: 2 }}>
+			<Box sx={{ m: 2, display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
 				<Typography variant="h4" component="h4">
 					Kopi
 				</Typography>
+				<Box sx={{ width: '100%' }} />
+				<IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+					{theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+				</IconButton>
 			</Box>
 			<Divider variant="middle" />
 			<Container sx={{ width: '40em' }}>
 				<Box sx={{ m: 2, display: 'flex', justifyContent: 'center' }}>
-					<Tracker setRefresh={setRefresh}/>
+					<Tracker setRefresh={setRefresh} />
 				</Box>
 				<Box sx={{ m: 2, display: 'flex', justifyContent: 'center' }}>
 					<Grid container spacing={2}>
