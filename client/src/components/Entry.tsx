35f918058fa8cc529924b817829af38ff2b489a8
Abhiraj Shourya <abhirajshourya@gmail.com>
2023-06-03 02:06:52 +0530

    UI Update

--- client/src/components/Entry.tsx
+++ client/src/components/Entry.tsx
@@ -1,4 +1,4 @@
-import { Delete } from '@mui/icons-material';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import { Card, CardActions, CardHeader, Chip, Grid, IconButton, Theme } from '@mui/material';
 import { deleteEntry } from '../routes/routes';
 import { TimeEntryModel } from '../common/TimeEntryModel';
@@ -80,7 +80,7 @@ function Entry({ tag, duration, id, setEntries, setRefresh, theme }: EntryProps)
 									});
 								}}
 							>
-								<Delete />
+								<CloseRoundedIcon />
 							</IconButton>
 						</CardActions>
 					</Grid>
