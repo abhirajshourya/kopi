d4ddbc80af681a74ac3bccf55c00abc54f48717c
Mohammed Akbar Ahmed <mohammed.ahmed@everlytics.io>
2023-06-11 08:16:52 +0530

    START/PAUSE

--- client/src/components/Tracker.tsx
+++ client/src/components/Tracker.tsx
@@ -1,62 +1,106 @@
-import React from 'react';
-import { formatTimestamp } from '../utils/formatTime';
+import React from "react";
+import { formatTimestamp } from "../utils/formatTime";
 import {
-	Button,
-	Card,
-	CardContent,
-	Divider,
-	Grid,
-	TextField,
-	Typography,
-	Box,
-} from '@mui/material';
-import { useTracker } from '../hooks/useTracker';
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  TextField,
+  Typography,
+  Box,
+} from "@mui/material";
+import { useTracker } from "../hooks/useTracker";
 
 interface TrackerProps {
-	setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
+  setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 function Tracker({ setRefresh }: TrackerProps) {
-	const { duration, isTrackerRunning, tag, setTag, toggleTimeTracker } = useTracker(setRefresh);
+  const {
+    duration,
+    isTrackerRunning,
+    tag,
+    setTag,
+    isTrackerPause,
+    startTimer,
+    pauseTimer,
+    stopTimer,
+  } = useTracker(setRefresh);
 
-	return (
-		<Card raised={true} sx={{ width: '32em' }}>
-			<Box sx={{ m: 2, height: '100%' }}>
-				<CardContent>
-					<Box sx={{ display: 'flex', justifyContent: 'center' }}>
-						<Typography variant="h2" component="h2">
-							{formatTimestamp(duration)}
-						</Typography>
-					</Box>
-					<Divider variant="middle" />
-					<Grid container alignItems="center" justifyContent="space-evenly" marginTop="1em">
-						<Grid item>
-							<TextField
-								label="Tag"
-								size="small"
-								variant="outlined"
-								value={tag}
-								onChange={(e) => {
-									setTag(e.target.value);
-								}}
-								error={isTrackerRunning && !tag.length}
-							/>
-						</Grid>
-						<Grid item>
-							<Button
-								disabled={isTrackerRunning && !tag.length}
-								sx={{ width: '8em' }}
-								variant="outlined"
-								onClick={toggleTimeTracker}
-							>
-								{isTrackerRunning ? 'Stop' : 'Start'}
-							</Button>
-						</Grid>
-					</Grid>
-				</CardContent>
-			</Box>
-		</Card>
-	);
+  return (
+    <Card raised={true} sx={{ width: "32em" }}>
+      <Box sx={{ m: 2, height: "100%" }}>
+        <CardContent>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Typography variant="h2" component="h2">
+              {formatTimestamp(duration)}
+            </Typography>
+          </Box>
+          <Divider variant="middle" />
+          <Grid
+            container
+            alignItems="center"
+            justifyContent="space-evenly"
+            marginTop="1em"
+          >
+            <Grid item>
+              <TextField
+                label="Tag"
+                size="small"
+                variant="outlined"
+                value={tag}
+                onChange={(e) => {
+                  setTag(e.target.value);
+                }}
+                error={isTrackerRunning && !tag.length}
+              />
+            </Grid>
+            <Grid item style={{ marginTop: "1rem" }}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "360px",
+                }}
+              >
+                <Button
+                  color="success"
+                  disabled={isTrackerRunning && !isTrackerPause}
+                  sx={{ width: "8em" }}
+                  variant="outlined"
+                  onClick={startTimer}
+                >
+                  START
+                </Button>
+                <Button
+                  color="primary"
+                  disabled={isTrackerPause || !isTrackerRunning}
+                  sx={{ width: "8em" }}
+                  variant="outlined"
+                  onClick={pauseTimer}
+                >
+                  PAUSE
+                </Button>
+                <Button
+                  color="error"
+                  variant="outlined"
+                  disabled={
+                    (isTrackerRunning && !tag.length) ||
+                    (!isTrackerRunning && !tag.length)
+                  }
+                  sx={{ width: "8em" }}
+                  onClick={stopTimer}
+                >
+                  STOP
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Box>
+    </Card>
+  );
 }
 
 export default Tracker;
--- client/src/hooks/useTracker.tsx
+++ client/src/hooks/useTracker.tsx
@@ -1,52 +1,101 @@
-import { useState } from 'react';
-import { createEntry } from '../routes/routes';
-import useTagInput from '../hooks/useTagInput';
-import { TimeEntryModel } from '../common/TimeEntryModel';
+import { useState, useEffect } from "react";
+import { createEntry } from "../routes/routes";
+import useTagInput from "../hooks/useTagInput";
+import { TimeEntryModel } from "../common/TimeEntryModel";
 
-export function useTracker(setRefresh: React.Dispatch<React.SetStateAction<boolean>>) {
-	const [duration, setDuration] = useState(0);
-	const [isTrackerRunning, setIsTrackerRunning] = useState(false);
-	const [timerId, setTimerId] = useState(0);
-	const [tag, setTag] = useTagInput('');
+export function useTracker(
+  setRefresh: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  const [duration, setDuration] = useState(0);
+  const [startTime, setStartTime] = useState(0);
+  const [isTrackerRunning, setIsTrackerRunning] = useState(false);
+  const [isTrackerPause, setIsTrackerPause] = useState(false);
+  const [timerId, setTimerId] = useState(0);
+  const [tag, setTag] = useTagInput("");
 
-	let TimeTracked: TimeEntryModel = {
-		duration,
-		tag,
-	};
+  let TimeTracked: TimeEntryModel = {
+    duration,
+    tag,
+  };
 
-	function toggleTimeTracker() {
-		if (isTrackerRunning) {
-			TimeTracked = {
-				...TimeTracked,
-				duration: duration,
-				tag: tag,
-			};
-			setDuration(0);
-			setTimerId(0);
-			setIsTrackerRunning(false);
-			setTag('');
-			clearInterval(timerId);
+  const startTimer = () => {
+    setStartTime(Date.now());
+    setIsTrackerRunning(true);
+    setIsTrackerPause(false);
+  };
 
-			createEntry(TimeTracked);
+  const pauseTimer = () => {
+    setStartTime(Date.now());
+    setIsTrackerPause(true);
+  };
 
-			setRefresh((prev) => !prev);
-		} else {
-			const startTimestamp = Date.now() - duration;
-			const id = setInterval(() => {
-				const currentDuration = Date.now() - startTimestamp;
-				setDuration(currentDuration);
-			}, 1000);
+  const stopTimer = () => {
+    setIsTrackerRunning(false);
+    setIsTrackerPause(false);
+    setDuration(0);
+    TimeTracked = {
+      ...TimeTracked,
+      duration: duration,
+      tag: tag,
+    };
+    setTimerId(0);
+    setTag("");
+    createEntry(TimeTracked);
+    setRefresh((prev) => !prev);
+  };
 
-			setTimerId(id);
-			setIsTrackerRunning(true);
-		}
-	}
+  useEffect(() => {
+    let timer: any = null;
+    if (isTrackerRunning && !isTrackerPause) {
+      timer = setInterval(() => {
+        setDuration((duration) => duration + 1000);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isTrackerRunning, isTrackerPause]);
 
-	return {
-		duration,
-		isTrackerRunning,
-		tag,
-		setTag,
-		toggleTimeTracker,
-	};
+  function toggleTimeTracker() {
+    if (isTrackerRunning) {
+      TimeTracked = {
+        ...TimeTracked,
+        duration: duration,
+        tag: tag,
+      };
+      setDuration(0);
+      setTimerId(0);
+      setIsTrackerRunning(false);
+      setTag("");
+      clearInterval(timerId);
+
+      createEntry(TimeTracked);
+
+      setRefresh((prev) => !prev);
+    } else {
+      const startTimestamp = Date.now() - duration;
+      const id = setInterval(() => {
+        const currentDuration = Date.now() - startTimestamp;
+        setDuration(currentDuration);
+      }, 1000);
+
+      setTimerId(id);
+      setIsTrackerRunning(true);
+    }
+  }
+
+  return {
+    duration,
+    isTrackerRunning,
+    isTrackerPause,
+    toggleTimeTracker,
+    tag,
+    setTag,
+    startTime,
+    startTimer,
+    pauseTimer,
+    stopTimer,
+  };
 }
